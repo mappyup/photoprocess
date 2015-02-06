6b06a2f21c7456035605f3461f9b9e20f6206f6c
mappyup <mappyup@gmail.com>
2015-02-05 23:08:47 -0500

    Update photoprocess.sh
    First commit was not correct.

--- photoprocess.sh
+++ photoprocess.sh
@@ -1,22 +1,3 @@
-#!/bin/bash
+#!/bin/sh
+exiftool "-Directory<DateTimeOriginal" -d "%Y-%m-%d" ./
 
-for f in "$@"
-do
-	exiftool -CreateDate $f &> /dev/null
-	if [ $? -gt 0 ]; then
-		echo "[ERR] file $f doesn't have EXIF data."
-	else
-		year=$(exiftool -CreateDate $f | cut -d ':' -f2 | tr -d ' ')
-		month=$(exiftool -CreateDate $f | cut -d ':' -f3 | tr -d ' ') 
-		day=$(exiftool -CreateDate $f | cut -d ':' -f4 | tr -d ' ' | cut -c1,2)
-
-		foldername="${year}-${month}-${day}" 
-	
-		if [ -e $foldername ]; then
-			echo "${foldername} is exist."
-		else
-			mkdir $foldername
-		fi
-		mv $f ./$foldername/$f
-	fi
-done
